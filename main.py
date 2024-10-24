644854696df4053ef9c142b9e2d2766ee62978f3
Markus Zondler <contact@markuszondler.com>
2024-10-24 23:03:23 +0200

    feat: sort chars count dictionary and print report

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-    print(f"{num_words} words found in document {book_path}")
-    print(chars_dict)
+    chars_list_sorted = get_chars_dict_sorted_num(chars_dict)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for char in chars_list_sorted:
+        if not char["char"].isalpha():
+            continue
+        c = char["char"]
+        n = char["num"]
+        print(f"The '{c}' character was found {n} times")
+
+    print("--- End report ---")
 
 
 def get_num_words(text):
@@ -21,6 +32,18 @@ def get_chars_dict(text):
         else:
             chars_dict[lower] = 1
     return chars_dict
+
+
+def sort_on_key_num(dict):
+    return dict["num"]
+
+
+def get_chars_dict_sorted_num(dict):
+    tmp_list = []
+    for char in dict:
+        tmp_list.append({"char": char, "num": dict[char]})
+    tmp_list.sort(reverse=True, key=sort_on_key_num)
+    return tmp_list
 
 
 def get_book_text(path):
