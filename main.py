5b4bfbbea36a16cab5409311ccb8975ad1b97899
Markus Zondler <contact@markuszondler.com>
2024-10-24 22:41:36 +0200

    feat: add get_chars_dict function

--- main.py
+++ main.py
@@ -2,12 +2,25 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
     print(f"{num_words} words found in document {book_path}")
+    print(chars_dict)
 
 
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+
+def get_chars_dict(text):
+    chars_dict = {}
+    for c in text:
+        lower = c.lower()
+        if lower in chars_dict:
+            chars_dict[lower] += 1
+        else:
+            chars_dict[lower] = 1
+    return chars_dict
 
 
 def get_book_text(path):
